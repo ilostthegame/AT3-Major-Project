a9265ad8879a3cc90e1db241b6624cda54c8444f
ilostthegame <liu_wi1@student.kings.edu.au>
2025-05-21 12:29:56 +1000

    Added Signup page
    - Added SignupForm to forms.py
    - Created signup.html template.
    - Added signup route in routes.py
    - Added empty-form error display to login.html
    - Changed route in base.html to use url_for()

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -6,3 +6,10 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,6 +1,6 @@
 from flask_app import app
-from flask import redirect, url_for, render_template
-from flask_app.forms import LoginForm
+from flask import redirect, url_for, render_template, flash
+from flask_app.forms import LoginForm, SignupForm
 
 user = {'username': 'Bob Dylan'}
 
@@ -10,16 +10,23 @@ def index():
     return redirect(url_for('login'))
     # TODO: Insert conditional for redirect based on user's authentication status.
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Handles user login"""
-
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(form.username.data))
+        return render_template('login.html', title='Sign In', form=form)
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     """Handles user signup"""
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash('Signup requested for user {}'.format(form.username.data))
+        return render_template('signup.html', title='Sign Up', form=form)
+    return render_template('signup.html', title='Sign Up', form=form)
 
 @app.route('/calendar')
 def calendar():
